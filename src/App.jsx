c68abed001a0c279c3ff6d72bed31e203677bc04
vickysawant19 <vicky.sawant19@gmail.com>
2024-07-07 11:11:58 +0530

    clone update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Outlet, useNavigate } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import { addUser } from './store/userSlice';
-import authService from './appwrite/auth';
-import { ClipLoader } from 'react-spinners';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { addUser } from "./store/userSlice";
+import authService from "./appwrite/auth";
+import { ClipLoader } from "react-spinners";
 
 function App() {
-  const user = useSelector(state => state.user);
+  const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = React.useState(true);
-  const navigate =  useNavigate()
-
+  const navigate = useNavigate();
 
   useEffect(() => {
     const checkUserStatus = async () => {
       try {
-        if(!user){
-        const res = await authService.getCurrentUser();
-        if (res) {
-          dispatch(addUser(res));
-          if (window.location.pathname === '/') {
-            navigate('/dash');
+        if (!user) {
+          const res = await authService.getCurrentUser();
+          if (res) {
+            dispatch(addUser(res));
+            if (window.location.pathname === "/") {
+              navigate("/dash");
+            }
           }
         }
-      }
       } catch (error) {
-        // console.log(error.message);
-        navigate('/login')
+        // console.log(error.message);     // log
+        navigate("/login");
       } finally {
         setIsLoading(false);
       }
@@ -38,20 +37,21 @@ function App() {
   }, [dispatch]);
 
   if (isLoading) {
-    return (<>
-      <Navbar/>
-      <div className="flex items-center justify-center min-h-screen">
-        <ClipLoader size={50} color={"#123abc"} loading={isLoading} />
-      </div>
-    </>
+    return (
+      <>
+        <Navbar />
+        <div className="flex items-center justify-center min-h-screen">
+          <ClipLoader size={50} color={"#123abc"} loading={isLoading} />
+        </div>
+      </>
     );
   }
 
   return (
-    <div className='bg-orange-50 w-full min-h-screen '>
+    <div className="bg-orange-50 w-full min-h-screen ">
       <Navbar />
-      <div className='pt-7'>
-      <Outlet />
+      <div className="pt-7">
+        <Outlet />
       </div>
       <Footer />
     </div>
