2ffd8afbd180064e68af8e6bb4b2605bfe1bfae2
vickysawant19 <vicky.sawant19@gmail.com>
2024-07-09 22:43:48 +0530

    as

--- functions/fetchTop.js
+++ functions/fetchTop.js
@@ -1,4 +1,4 @@
-const sdk = require("node-appwrite");
+// const sdk = require("node-appwrite");
 const { appwriteService } = require("../src/appwrite/appwriteConfig");
 
 module.exports = async function (req, res) {
