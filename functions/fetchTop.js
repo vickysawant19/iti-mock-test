67a1443254ddacc87af28e1a7dbbfed7c186a315
vickysawant19 <vicky.sawant19@gmail.com>
2024-07-10 00:57:38 +0530

    asa

--- functions/fetchTop.js
+++ functions/fetchTop.js
@@ -1,4 +1,4 @@
-import { Client, Databases } from "node-appwrite";
+import { Client, Databases, Query } from "node-appwrite";
 
 export default async ({ req, res, log, error }) => {
   const client = new Client();
@@ -10,36 +10,55 @@ export default async ({ req, res, log, error }) => {
   const database = new Databases(client);
 
   try {
-    const startDate = new Date();
-    startDate.setHours(0, 0, 0, 0); // Change this to get the top users for day, week, month as needed
+    const today = new Date();
+    const startOfDay = new Date(today.setHours(0, 0, 0, 0)).toISOString();
+    const startOfWeek = new Date(
+      today.setDate(today.getDate() - today.getDay())
+    ).toISOString();
+    const startOfMonth = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      1
+    ).toISOString();
 
-    // Fetch questions from the database
-    const questions = await database.listDocuments(
-      process.env.APPWRITE_DATABASE_ID,
-      process.env.APPWRITE_QUES_COLLECTION_ID
+    const getContributers = async (startDate) => {
+      // Fetch questions from the database
+      const questions = await database.listDocuments(
+        process.env.APPWRITE_DATABASE_ID,
+        process.env.APPWRITE_QUES_COLLECTION_ID,
+        [Query.greaterThanEqual("$createdAt", startDate)]
+      );
+
+      // Calculate the number of questions created by each user
+      const userQuestionsCount = questions.documents.reduce((acc, doc) => {
+        if (acc[doc.userId]) {
+          acc[doc.userId].questionsCount += 1;
+        } else {
+          acc[doc.userId] = {
+            userName: doc.userName,
+            userId: doc.userId,
+            questionsCount: 1,
+          };
+        }
+        return acc;
+      }, {});
+      // Convert userQuestionsCount object to array and sort by the number of questions in descending order
+      const sortedUsers = Object.values(userQuestionsCount)
+        .sort((a, b) => b.questionsCount - a.questionsCount)
+        .slice(0, 5);
+
+      return sortedUsers;
+    };
+
+    const dayContributers = getContributers(startOfDay);
+    const weeekContributers = getContributers(startOfWeek);
+    const monthContributers = getContributers(startOfMonth);
+
+    log(
+      JSON.stringify({ dayContributers, monthContributers, weeekContributers })
     );
 
-    // Calculate the number of questions created by each user
-    const userQuestionsCount = questions.documents.reduce((acc, doc) => {
-      if (acc[doc.userId]) {
-        acc[doc.userId].questionsCount += 1;
-      } else {
-        acc[doc.userId] = {
-          userName: doc.userName,
-          userId: doc.userId,
-          questionsCount: 1,
-        };
-      }
-      return acc;
-    }, {});
-
-    // Convert userQuestionsCount object to array and sort by the number of questions in descending order
-    const sortedUsers = Object.values(userQuestionsCount)
-      .sort((a, b) => b.questionsCount - a.questionsCount)
-      .slice(0, 5);
-
-    log(JSON.stringify(sortedUsers));
-    return res.send(sortedUsers);
+    return res.send({ dayContributers, monthContributers, weeekContributers });
   } catch (err) {
     error(err);
     return res.json({ error: err.message });
