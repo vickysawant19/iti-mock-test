cb9e07829275c0a82628f1fb783100beb3364f80
vickysawant19 <vicky.sawant19@gmail.com>
2024-07-09 22:41:31 +0530

    aa

--- functions/fetchTop.js
+++ functions/fetchTop.js
@@ -1,13 +1,17 @@
-// const sdk = require("node-appwrite");
-// const { appwriteService } = require("../src/appwrite/appwriteConfig");
+const sdk = require("node-appwrite");
+const { appwriteService } = require("../src/appwrite/appwriteConfig");
 
 module.exports = async function (req, res) {
-  //   const database = appwriteService.getDatabases();
-
   try {
-    console.log(req.headers);
+    // Ensure req is defined
+    if (!req || !req.headers) {
+      throw new Error("Request object is not defined or missing headers");
+    }
+
+    context.log(req.headers);
     res.json({ message: "Got the function data" });
   } catch (error) {
-    console.log(error);
+    context.error(error);
+    res.status(500).json({ error: error.message });
   }
 };
