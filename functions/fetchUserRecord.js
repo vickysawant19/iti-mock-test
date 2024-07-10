2eaf8ae60f0712f603c896805afe3b4ddc7ee027
vickysawant19 <vicky.sawant19@gmail.com>
2024-07-10 10:15:07 +0530

    s

--- functions/fetchUserRecord.js
+++ functions/fetchUserRecord.js
@@ -9,8 +9,7 @@ export default async ({ req, res, log, error }) => {
 
   const database = new Databases(client);
 
-  const { userId } = req.body;
-  log(req.body);
+  const userId = req.body;
 
   try {
     const today = new Date();
