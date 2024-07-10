c8a137e72d1c48cb64b42cec41c69935f4522345
vickysawant19 <vicky.sawant19@gmail.com>
2024-07-10 09:36:35 +0530

    as

--- functions/fetchTop.js
+++ functions/fetchTop.js
@@ -97,68 +97,6 @@ export default async ({ req, res, log, error }) => {
       month: getContributionData(startOfMonth),
     };
 
-    // const getContributorsAndScorers = async (startDate) => {
-    //   // Fetch all necessary data in one call
-
-    //   const [questions, mockTests] = await Promise.all([
-    //     database.listDocuments(
-    //       process.env.APPWRITE_DATABASE_ID,
-    //       process.env.APPWRITE_QUES_COLLECTION_ID,
-    //       [Query.greaterThanEqual("$createdAt", startDate)]
-    //     ),
-    //     database.listDocuments(
-    //       process.env.APPWRITE_DATABASE_ID,
-    //       process.env.QUESTIONPAPER_COLLECTION_ID,
-    //       [Query.greaterThanEqual("$createdAt", startDate)]
-    //     ),
-    //   ]);
-    // Calculate the number of questions created by each user
-    //   const userQuestionsCount = questions.documents.reduce((acc, doc) => {
-    //     if (acc[doc.userId]) {
-    //       acc[doc.userId].questionsCount += 1;
-    //     } else {
-    //       acc[doc.userId] = {
-    //         userName: doc.userName,
-    //         userId: doc.userId,
-    //         questionsCount: 1,
-    //       };
-    //     }
-    //     return acc;
-    //   }, {});
-
-    // Calculate the top scorers
-    //   const userScores = mockTests.documents.reduce((acc, doc) => {
-    //     if (acc[doc.userId]) {
-    //       acc[doc.userId].score =
-    //         Math.max(doc.score, acc[doc.userId].score) || 0;
-    //     } else {
-    //       acc[doc.userId] = {
-    //         userName: doc.userName,
-    //         userId: doc.userId,
-    //         score: doc.score || 0,
-    //       };
-    //     }
-    //     return acc;
-    //   }, {});
-
-    // Convert to arrays and sort
-    //   const sortedContributors = Object.values(userQuestionsCount)
-    //     .sort((a, b) => b.questionsCount - a.questionsCount)
-    //     .slice(0, 5);
-
-    //   const sortedScorers = Object.values(userScores)
-    //     .sort((a, b) => b.score - a.score)
-    //     .slice(0, 5);
-
-    //   return { contributors: sortedContributors, scorers: sortedScorers };
-    // };
-
-    // const topContributors = {
-    //   day: await getContributorsAndScorers(startOfDay),
-    //   week: await getContributorsAndScorers(startOfWeek),
-    //   month: await getContributorsAndScorers(startOfMonth),
-    // };
-
     log(JSON.stringify({ topContributors }));
 
     return res.json({ topContributors });
