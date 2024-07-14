e3911078870827311178ef07c98fd6758cf899b3
vickysawant19 <vicky.sawant19@gmail.com>
2024-07-14 23:55:51 +0530

    asa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Noto Sans Devanagari", "sans-serif"], // Default sans-serif font
+        marathi: ["Noto Sans Devanagari", "sans-serif"], // Marathi font
+        hindi: ["Noto Sans Devanagari", "sans-serif"], // Hindi font
+      },
+    },
   },
   plugins: [],
-}
+};
