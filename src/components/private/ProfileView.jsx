e8c8e54b896682a36eb4771226319a875bc95280
vickysawant19 <vicky.sawant19@gmail.com>
2024-08-02 12:47:18 +0530

    update profile college name

--- src/components/private/ProfileView.jsx
+++ src/components/private/ProfileView.jsx
@@ -3,35 +3,68 @@ import { useSelector } from "react-redux";
 import { format } from "date-fns";
 import tradeservice from "../../appwrite/tradedetails";
 import batchService from "../../appwrite/batchService";
+import collegeService from "../../appwrite/collageService";
+import { ClipLoader } from "react-spinners";
 
 const ProfileView = ({ profile }) => {
   const user = useSelector((state) => state.user);
 
   const [tradedata, setTradeData] = useState([]);
   const [batches, setBatches] = useState([]);
+  const [colleges, setColleges] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchTrades = async () => {
+    setIsLoading(true);
     try {
       const res = await tradeservice.listTrades();
       setTradeData(res.documents);
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   const fetchBatches = async () => {
+    setIsLoading(true);
     try {
       const res = await batchService.listBatches();
       setBatches(res.documents);
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const fetchColleges = async () => {
+    setIsLoading(true);
+    try {
+      const res = await collegeService.listColleges();
+      setColleges(res.documents);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
     fetchTrades();
     fetchBatches();
+    fetchColleges();
   }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <div className="flex w-full h-full items-center justify-center">
+          <ClipLoader color="#123abc" size={50} />
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
@@ -52,11 +85,21 @@ const ProfileView = ({ profile }) => {
           <p className="text-gray-800">{user.phone}</p>
         </div>
         <div>
+          <h2 className="text-lg font-semibold text-gray-600">College</h2>
+          <p className="text-gray-800 capitalize ">
+            {colleges &&
+              colleges
+                .find((clg) => clg.$id === profile.collegeId)
+                ?.collageName.toLowerCase()}
+          </p>
+        </div>
+        <div>
           <h2 className="text-lg font-semibold text-gray-600">Trade</h2>
-          <p className="text-gray-800">
+          <p className="text-gray-800 capitalize">
             {tradedata &&
-              tradedata.find((trade) => trade.$id === profile.tradeId)
-                ?.tradeName}
+              tradedata
+                .find((trade) => trade.$id === profile.tradeId)
+                ?.tradeName.toLowerCase()}
           </p>
         </div>
         <div>
