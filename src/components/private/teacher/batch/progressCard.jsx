c05fa68b3dfa829643efcdc3caede346c9876442
vickysawant19 <vicky.sawant19@gmail.com>
2025-02-20 19:23:42 +0530

    as

--- src/components/private/teacher/batch/progressCard.jsx
+++ src/components/private/teacher/batch/progressCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, lazy } from "react";
 import { format } from "date-fns";
 import { Printer } from "lucide-react";
 import {
